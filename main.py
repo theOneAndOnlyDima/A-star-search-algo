23870838707cbdd314306ed282d9da59b70b457e
Dima <zlotea_dumitru@yahoo.com>
2023-11-03 00:29:29 -0400

    added some colors

--- main.py
+++ main.py
@@ -12,7 +12,19 @@ end_point = ''
 # create input rectangle
 start_input_rect = pygame.Rect(200, 200, 140, 32)
 
-color = pygame.Color('darkgray')
+# vars
+color = pygame.Color(80, 125, 188)
+background_color = pygame.Color(4, 8, 15)
+button_color = pygame.Color(42, 67, 102)
+input_box_color = pygame.Color(187, 209, 234)
+input_window_color = pygame.Color()
+grid_color = pygame.Color(23, 38, 59)
+start_end_point_color = pygame.Color()
+wall_color = pygame.Color()
+all_paths_color = pygame.Color()
+shortest_path_color = pygame.Color()
+# https://coolors.co/04080f-17263b-213551-2a4366-507dbc-a1c6ea-bbd1ea
+
 
 while running:
     # poll for events
