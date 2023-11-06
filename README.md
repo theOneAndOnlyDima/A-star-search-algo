5e765e3f30349d950efad6cb66d96455ecfc4388
Dima <zlotea_dumitru@yahoo.com>
2023-11-06 01:23:06 -0500

    finished choosing colors and started coord input window

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Visualisation of the a-star path finding algorithm using python
 
 1. [ ] Create input box in middle of screen to get start and end points 
 and store them in variables
-2. [ ] Choose better colors
+2. [x] Choose better colors
 3. [ ] Create function to verify input
 4. [ ] Make background grid
+5. [ ] Create function to randomize coords
--- main.py
+++ main.py
@@ -2,27 +2,36 @@ import pygame
 
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+width = 1280
+height = 720
+screen = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
 running = True
 
-start_point = ''
-end_point = ''
 
-# create input rectangle
-start_input_rect = pygame.Rect(200, 200, 140, 32)
+class Coords:
+    start = [0, 0]
+    end = [0, 0]
+
+
+# display window to ask for start and end coords
+# TODO: fix the dimensions
+def ask_coords_window() -> Coords:
+    input_window = pygame.Rect(width/2-150, height/2-100, 350, 200)
+    pygame.draw.rect(screen, input_window_color, input_window)
+    pass
+
 
 # vars
-color = pygame.Color(80, 125, 188)
 background_color = pygame.Color(4, 8, 15)
-button_color = pygame.Color(42, 67, 102)
+button_color = pygame.Color(80, 125, 188)
 input_box_color = pygame.Color(187, 209, 234)
-input_window_color = pygame.Color()
+input_window_color = pygame.Color(42, 67, 102)
 grid_color = pygame.Color(23, 38, 59)
-start_end_point_color = pygame.Color()
-wall_color = pygame.Color()
-all_paths_color = pygame.Color()
-shortest_path_color = pygame.Color()
+start_end_point_color = pygame.Color(69, 40, 230)
+wall_color = pygame.Color(211, 24, 57)
+all_paths_color = pygame.Color(24, 190, 182)
+shortest_path_color = pygame.Color(13, 179, 17)
 # https://coolors.co/04080f-17263b-213551-2a4366-507dbc-a1c6ea-bbd1ea
 
 
@@ -36,7 +45,7 @@ while running:
     # fill the screen with a color to wipe away anything from last frame
     screen.fill((95, 95, 95))
 
-    pygame.draw.rect(screen, color, start_input_rect)
+    ask_coords_window()
 
     # flip() the display to put your work on screen
     pygame.display.flip()
