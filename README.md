cfd0adb361129dc5c1f8fd87143b02619a05df5b
Dima <zlotea_dumitru@yahoo.com>
2023-11-17 13:04:32 -0500

    walls drawing finished

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
-# A* path finding algorithm
+# A\* path finding algorithm
+
 Visualisation of the a-star path finding algorithm using python
 
 ## Currently working on:
 
-1. [ ] Create input box in middle of screen to get start and end points 
-and store them in variables
+1. [x] Create input box in middle of screen to get start and end points
+       and store them in variables
 2. [x] Choose better colors
-3. [ ] Create function to verify input
-4. [ ] Make background grid
+3. [x] Create function to verify input
+4. [x] Make background grid
 5. [ ] Create function to randomize coords
+6. [ ] draw walls
--- main.py
+++ main.py
@@ -64,15 +64,33 @@ for i in range(0, rows):
     grid[i][cols-1].show(input_window_color)
 
 
+def verify_coords(start, end) -> bool:
+    if len(start) != 2 or len(end) != 2:
+        return False
+    if not start[0].isdigit() or not start[1].isdigit() or not end[0].isdigit() or not end[1].isdigit():
+        return False
+    if int(start[0]) < 1 or int(start[0]) > 48 or int(start[1]) < 1 or \
+        int(start[1]) > 48 or int(end[0]) < 1 or int(end[0]) > 48 or int(end[1]) < 1 or int(end[1]) > 48:
+        return False
+    return True
+
+
 def on_submit():
     global start
     global end
     s = entry_start.get().split(",")
     e = entry_end.get().split(",")
-    start = grid[int(s[0])][int(s[1])]
-    end = grid[int(e[0])][int(e[1])]
-    window.quit()
-    window.destroy()
+    if verify_coords(s, e) and s != e:
+        start = grid[int(s[0])][int(s[1])]
+        end = grid[int(e[0])][int(e[1])]
+        window.quit()
+        window.destroy()
+    elif s != e:
+        label_error.config(text="Only digits between 1 and 48 are allowed!",
+                           foreground="red")
+    else:
+        label_error.config(text="Start and end coordinates must be different!",
+                           foreground="red")
 
 
 window = Tk()
@@ -81,12 +99,14 @@ window.title("Coordinates")
 window_width = window.winfo_screenwidth()
 window_height = window.winfo_screenheight()
 
-window.geometry(f"270x80+{window_width//2-135}+{window_height//2-40}")
+window.geometry(f"270x95+{window_width//2-135}+{window_height//2-40}")
 label_start = Label(window, text="Start coordinates (x,y):")
 label_end = Label(window, text="End coordinates (x,y):")
+label_error = Label(window, text = "")
 
 label_start.grid(row=0, column=0)
 label_end.grid(row=1, column=0)
+label_error.grid(row = 3, columnspan=2)
 
 entry_start = Entry(window)
 entry_end = Entry(window)
@@ -106,13 +126,31 @@ pygame.init()
 start.show(start_end_point_color)
 end.show(start_end_point_color)
 
-
-# display window to ask for start and end coords
-# TODO: study the algorithm
-# TODO: check if input is in the desired range (1,48)
-
-def verify_coords() -> bool:
-    pass
+valid = True
+while valid:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            valid = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if pygame.mouse.get_pressed()[0]:
+                pos = pygame.mouse.get_pos()
+                x = pos[0] // w
+                y = pos[1] // h
+                if grid[x][y] != start and grid[x][y] != end and not grid[x][y].wall:
+                    grid[x][y].wall = True
+                    grid[x][y].show(wall_color)
+        if event.type == pygame.MOUSEMOTION and pygame.mouse.get_pressed()[0]:
+            pos = pygame.mouse.get_pos()
+            x = pos[0] // w
+            y = pos[1] // h
+            if grid[x][y] != start and grid[x][y] != end and not grid[x][y].wall:
+                grid[x][y].wall = True
+                grid[x][y].show(wall_color)
+        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE or event.key == pygame.K_RETURN:
+                valid = False
 
 def main():
     start.show(start_end_point_color)
